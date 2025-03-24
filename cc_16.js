74c0d04c64c3cd952dd6e844b534fe6f2aac1e76
Gabriela Serrano <gabrielasc0309@gmail.com>
2025-03-24 18:03:43 -0400

    Implement fetchProductsThen using .then() and .catch()

--- cc_16.js
+++ cc_16.js
@@ -1,4 +1,23 @@
 // Task 1: Set Up the Project Structure
 	//Set up the basic HTML structure in the HTML file
 
+// Task 2: Fetch products using .then() 
+function fetchProductsThen() {
+  // GET request to the API using fetch and handle with .then()
+  fetch("https://www.course-api.com/javascript-store-products")
+    .then((response) => {
+      // Convert response to JSON
+      return response.json();
+    })
+    .then((products) => {
+      // Log name of each product to the console
+      products.forEach(product => {
+        console.log(product.fields.name);
+      });
+    })
+    .catch((error) => {
+      // Log errors that occur during the fetch
+      console.error("Error fetching with .then():", error);
+    });
+}
 
